6654db3695193da3b4bd88f124de6595ffcdf979
sergi aparicio <sergitus2525@gmail.com>
2022-09-14 22:41:31 +0200

    landingPage semiend

--- mama/src/pages/LandingPage.jsx
+++ mama/src/pages/LandingPage.jsx
@@ -2,25 +2,26 @@ const LandingPage = () => {
     return (
         <section className="flex text-white bg-center bg-no-repeat bg-cover h-screen w-full flex-col items-center bg-atrapasueños">
             <div className="flex w-full h-44 items-center justify-center flex-col">
-                <div className="flex font-semibold text-4xl h-20 w-full justify-center items-center">Chloe Fumet</div>
-                <div className="flex font-bold w-72 uppercase text-center">artesania y manualidades handmade</div>
+                <div className="flex font-semibold text-4xl h-20 w-full justify-center items-center font-petit">Chloe Fumet</div>
+                <div className="flex font-bold w-72 uppercase text-center font-permanent">artesania y manualidades handmade</div>
             </div>
-            <div className="flex w-full h-full items-center">
+            <div className="flex w-full h-full items-center justify-center">
+              <div className="flex w-80 ">
             <div id="carouselExampleCaptions" class="carousel slide relative" data-bs-ride="carousel">
   <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner relative w-full overflow-hidden">
+  <div class="carousel-inner relative w-full overflow-hidden h-56">
     <div class="carousel-item active relative float-left w-full">
-      <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg" class="block w-full" alt="..." />
+      <img src="https://i.ibb.co/2cmHVCQ/IMG-20220808-WA0001.jpg" class="flex block overflow-hidden" alt="..." />
       <div class="carousel-caption hidden md:block absolute text-center">
         <h5 class="text-xl">First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
-    <div class="carousel-item relative float-left w-full">
+    <div class="carousel-item relative float-left w-full h-44">
       <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg" class="block w-full" alt="..." />
       <div class="carousel-caption hidden md:block absolute text-center">
         <h5 class="text-xl">Second slide label</h5>
@@ -44,17 +45,17 @@ const LandingPage = () => {
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-            </div>
-            <div className="flex w-full h-44 items-center justify-center text-center uppercase">
+</div>
+</div>
+            <div className="flex w-full h-44 items-center justify-center text-center uppercase font-bold ">
                 <div>
-                    <ul>
-                        <li>contacto:</li>
+                    <ul className="font-permanent">
+                        <li className="font-paprika">contacto:</li>
                         <li>telefono: 683 23 32 12</li>
                         <li>email: atrapasomnis.ribes@gmail.com</li>
                     </ul>
                 </div>
                 <div>
-
                 </div>
             </div>
         </section>
--- mama/tailwind.config.js
+++ mama/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: [
     './src/**/*.{html,js,jsx,ts,tsx}', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
+    fontFamily: {
+      'petit': ['Petit Formal Script','cursive','sans-serif'],
+      'permanent': ['Permanent Marker','cursive','sans-serif'],
+      'paprika': ['Paprika','cursive','sans-serif']
+    },
     extend: {
       backgroundImage: {
         'atrapasueños': "url('./img/atrapasueños.jpg')",
